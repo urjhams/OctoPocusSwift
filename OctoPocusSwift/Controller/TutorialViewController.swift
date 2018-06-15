5c5dd5ff74ed5080dca7830ef8a0a4590fe18a1a
Quan <urjhams@gmail.com>
2018-06-15 23:56:46 +0200

    drawable viewcontroller

--- OctoPocusSwift/Controller/TutorialViewController.swift
+++ OctoPocusSwift/Controller/TutorialViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class TutorialViewController: UIViewController {
-
+    @IBOutlet weak var imgCanvas: UIImageView!
+    
+    var lastTouch = CGPoint.zero
+    var lastPoint: CGPoint?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchNumberOne = touches.first {
+            self.lastTouch = touchNumberOne.location(in: view)
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchNumberOne = touches.first {
+            let touchLocation = touchNumberOne.location(in: view)
+            self.lastPoint = touchLocation
+            print("\(lastPoint ?? CGPoint(x: 0, y: 0))\n")
+            drawLine(from: self.lastTouch, to: touchLocation)
+            self.lastTouch = touchLocation
+        }
+    }
+    private func drawLine(from start: CGPoint, to end: CGPoint) {
+        UIGraphicsBeginImageContext(imgCanvas.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.move(to: CGPoint(x: start.x, y: start.y))
+        context?.addLine(to: CGPoint(x: end.x, y: end.y))
+        context?.setLineCap(.round)
+        context?.setLineWidth(25)
+        context?.setStrokeColor(UIColor.black.cgColor)
+        context?.strokePath()
+        self.imgCanvas.image?.draw(in: view.frame)
+        imgCanvas.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+    }
 }
