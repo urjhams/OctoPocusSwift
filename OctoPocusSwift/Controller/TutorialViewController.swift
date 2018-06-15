4864965ffa6c6a6d08dca4a0792c0636c5cc42c5
Quan <urjhams@gmail.com>
2018-06-15 23:47:00 +0200

    Restructure

---
+++ OctoPocusSwift/Controller/TutorialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialViewController.swift
+//  OctoPocusSwift
+//
+//  Created by urjhams on 6/15/18.
+//  Copyright © 2018 urjhams. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
